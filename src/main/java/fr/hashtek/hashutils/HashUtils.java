f5f161f24e3a65459c6d70ca57421fb1138e2df4
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-28 19:50:54 +0200

    feat(hashutils): Finished getAimedPlayer() function.

--- src/main/java/fr/hashtek/hashutils/HashUtils.java
+++ src/main/java/fr/hashtek/hashutils/HashUtils.java
@@ -1,14 +1,13 @@
 package fr.hashtek.hashutils;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class HashUtils
 {
@@ -43,21 +42,25 @@ public class HashUtils
         return null;
     }
 
-    public static Player getAimedPlayer(Player player)
+    public static Player getAimedPlayer(Player player, int range)
     {
-        final Location playerEye = player.getEyeLocation();
+        final ArrayList<Entity> entities = (ArrayList<Entity>) player.getNearbyEntities(range, range, range);
+        final ArrayList<Block> sightBlock = (ArrayList<Block>) player.getLineOfSight( (Set<Material>) null, range);
+        final ArrayList<Location> sight = new ArrayList<Location>();
 
-        for (Entity entity : player.getNearbyEntities(3, 3, 3)) {
-            if (!(entity instanceof Player))
-                continue;
+        for (Entity e : entities)
+            if (!(e instanceof Player))
+                entities.remove(e);
 
-            final Player p = (Player) entity;
-            final Vector vector = p.getEyeLocation().toVector().subtract(playerEye.toVector());
-            final double dot = vector.normalize().dot(playerEye.getDirection());
+        for (int i = 0; i < sightBlock.size(); i++)
+            sight.add(sightBlock.get(i).getLocation());
 
-            if (dot > 0.99d)
-                return p;
-        }
+        for (int i = 0; i < sight.size(); i++)
+            for (int k = 0 ; k < entities.size() ;k++)
+                if (Math.abs(entities.get(k).getLocation().getX() - sight.get(i).getX()) < 1.3)
+                    if (Math.abs(entities.get(k).getLocation().getY() - sight.get(i).getY()) < 1.5)
+                        if (Math.abs(entities.get(k).getLocation().getZ() - sight.get(i).getZ()) < 1.3)
+                            return (Player) entities.get(k);
 
         return null;
     }
